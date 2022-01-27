3090bd4f1fa5b284addc79efa5668fddd97aea44
Yannick <yannickbokolopro@gmail.com>
2022-01-27 21:33:54 +0100

    Creation PostController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/articles', function () {
-    return view('articles');
-});
+Route::get('/', [PostController::class, 'index']);
